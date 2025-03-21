9c46f1fe167ec3edd8f00ad9990d63a6c5614f06
Kris Rowe <kris.rowe@anl.gov>
2025-03-21 18:08:47 -0500

    Updates build script to use next SDK

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -e -a
 
-module load cmake
+module -q purge
+module -q restore
+module -q swap mpich/opt/4.3.0rc3 mpich/opt/develop-git.7dd7b89 
+module -q unload oneapi/eng-compiler/2024.07.30.002
+module -q use /opt/aurora/24.180.3/spack/unified/0.8.0/install/modulefiles/oneapi/2024.07.30.002
+module -q use /soft/preview/pe/24.347.0-RC2/modulefiles
+module -q add oneapi/release/2025.0.5 
+module -q unload intel_compute_runtime 
+module -q load cmake
+module list
 
 CC=mpicc
 CXX=mpic++
